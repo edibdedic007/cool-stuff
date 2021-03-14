5d8520ae483ed4e81e66fb3d50e928e98c91b026
Edib Dedic <61050514+edibdedic007@users.noreply.github.com>
2021-03-14 12:34:46 +0100

    Delete src/doublyLinkedList directory

--- src/doublyLinkedList/Node.java
+++
@@ -1,9 +0,0 @@
-package doublyLinkedList;
-
-/*First we define the node*/
-
-public class Node<T> {
-    T data; //data
-    Node<T> prev; //reference to previous node
-    Node<T> next; //reference to next node
-}
